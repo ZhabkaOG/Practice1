61b820eb2f232a0fc027e2ffec96c0203e7512b5
ivc6-10 <mirea@gmail.com>
2020-09-03 11:08:58 +0300

    Инициализация проекта

---
+++ src/Loader.java
@@ -0,0 +1,14 @@
+public class Loader {
+    public static void main (String[]args){
+        double a, i, b;
+        b=0;
+       for(i=1;i<11;i++)
+       {
+           a=1/i;
+           b+=a;
+           System.out.println(a);
+       }
+        System.out.println(b);
+        }
+    }
+
